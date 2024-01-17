28327aed3b36cc8820d0be3fb9562e6a5989c3e4
917trancanaomi <naomi.tranca@stud.ubbcluj.ro>
2024-01-18 00:50:19 +0200

    small tries

--- ProjectPDP/ProjectPDP.cpp
+++ ProjectPDP/ProjectPDP.cpp
@@ -1,23 +1,32 @@
 #include<opencv2/opencv.hpp>
 #include<iostream>
 #include "DistributedTransform.h"
+#include "NormalThreadsTransform.h"
 
 using namespace std;
 using namespace cv;
 
+
+void testNormalThreadsTransform(string filename) {
+	int threadsNumber = 4;  // You can adjust the number of threads
+	int chunksNumber = 4;   // You can adjust the number of chunks
+	NormalThreadsTransform ntt(threadsNumber, chunksNumber);
+	ntt.gogoBoys(filename);
+}
 
 void testDistributedTransform(string filename) {
 	DistributedTransform dt;
 	dt.gogoBoys(filename);
 }
 
-
-
 int main()
 {
-	std::string filename("C:\\Users\\Oana\\source\\repos\\ProjectPDP\\image.png");
-	testDistributedTransform(filename);
+	std::string filename("C:\\Users\\Naomi\\Downloads\\example.png");
+	//testDistributedTransform(filename);
 
-	destroyAllWindows();
+	// Test Normal Threads Transform
+	testNormalThreadsTransform(filename);
+
+	//destroyAllWindows();
 	return 0;
 }
