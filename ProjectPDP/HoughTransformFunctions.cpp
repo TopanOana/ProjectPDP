e58dbc17b28ca1a2cc8dde4c97cb0b17ca247005
Oana Topan <91469778+TopanOana@users.noreply.github.com>
2024-01-18 10:06:09 +0200

    changed hough transform function

--- ProjectPDP/HoughTransformFunctions.cpp
+++ ProjectPDP/HoughTransformFunctions.cpp
@@ -1,13 +1,27 @@
 #include "HoughTransformFunctions.h"
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <iostream>
 
 using namespace std;
 using namespace cv;
 
+void polarToCartesian(double rho, int theta, Point& p1, Point& p2) {
+
+	int x0 = cvRound(rho * cos(theta));
+	int y0 = cvRound(rho * sin(theta));
+
+	p1.x = cvRound(x0 + 1000 * (-sin(theta)));
+	p1.y = cvRound(y0 + 1000 * (cos(theta)));
+
+	p2.x = cvRound(x0 - 1000 * (-sin(theta)));
+	p2.y = cvRound(y0 - 1000 * (cos(theta)));
+}
+
 cv::Mat houghLinesTransform(cv::Mat sourceImage)
 {
 	Mat imageWEdges;
+	int lineThreshold = 150;
 
 	//the edge detection
 	Canny(sourceImage, imageWEdges, 50, 200, 3, false);
@@ -22,23 +36,66 @@ cv::Mat houghLinesTransform(cv::Mat sourceImage)
 	// 1 -> resolution of accumulator
 	// CV_PI/180 -> angle resolution of accumulator
 	// 150 -> threshold for line detection
-	HoughLines(imageWEdges, lines, 1, CV_PI / 180, 150);
+	//changing times boy
+	//HoughLines(imageWEdges, lines, 1, CV_PI / 180, 150);
 
-	for (int x = 0; x < lines.size(); x++) {
-		double rho = lines[x][0];
-		double theta = lines[x][1];
+	//de aici 
+	int maxDistance = sqrt(imageWEdges.rows * imageWEdges.rows + imageWEdges.cols * imageWEdges.cols);
 
-		double a = cos(theta);
-		double b = sin(theta);
-		double x0 = a * rho;
-		double y0 = b * rho;
+	vector<vector<int>> votes(2 * maxDistance, vector<int>(NUMBINS, 0));
 
-		Point point1(cvRound(x0 + 1000 * (-b)), cvRound(y0 + 1000 * (a)));
-		Point point2(cvRound(x0 - 1000 * (-b)), cvRound(y0 - 1000 * (a)));
+	for (int i = 0; i < imageWEdges.rows; i++) {
+		for (int j = 0; j < imageWEdges.cols; j++) {
+			if (imageWEdges.at<uchar>(i, j) == 255) {
+				//cout << "in theta turn i=" << i << " j=" << j << endl;
+				for (int theta = 0; theta < 180; theta += BIN_WIDTH) {
 
-		line(imageWLines, point1, point2, Scalar(0, 0, 255), 3, LINE_AA, 0);
+					int rho = round(j * cos(theta - 90) + i * sin(theta - 90)) + maxDistance;
 
+					//cout << "rho = " << rho << " theta = " << theta << endl;
+
+					votes[rho][theta]++;
+				}
+			}
+
+
+		}
 	}
+
+	//pana aici pt cuda kernel
+
+	for (int i = 0; i < votes.size(); i++) {
+		for (int j = 0; j < votes[i].size(); j++) {
+			if (votes[i][j] >= lineThreshold) {
+				//cout << "in line threshold i=" << i << " j=" << j << endl;
+				int rho = i - maxDistance;
+				int theta = j - 90;
+
+				Point p1, p2;
+				polarToCartesian(rho, theta, p1, p2);
+
+				line(imageWLines, p1, p2, Scalar(0, 0, 255), 2, LINE_AA);
+			}
+		}
+	}
+
+
+
+	//for (int x = 0; x < lines.size(); x++) {
+	//	double rho = lines[x][0];
+	//	double theta = lines[x][1];
+
+	//	double a = cos(theta);
+	//	double b = sin(theta);
+	//	double x0 = a * rho;
+	//	double y0 = b * rho;
+
+	//	Point point1(cvRound(x0 + 1000 * (-b)), cvRound(y0 + 1000 * (a)));
+	//	Point point2(cvRound(x0 - 1000 * (-b)), cvRound(y0 - 1000 * (a)));
+
+	//	line(imageWLines, point1, point2, Scalar(0, 0, 255), 3, LINE_AA, 0);
+
+	//}
 
 	return imageWLines;
 }
--- ProjectPDP/HoughTransformFunctions.h
+++ ProjectPDP/HoughTransformFunctions.h
@@ -2,6 +2,12 @@
 #include <opencv2/core/base.hpp>
 #include <opencv2/core/mat.hpp>
 
+#define BIN_WIDTH 1
+#define NUMBINS 180 / BIN_WIDTH
+#define KERNEL_SIZE 3
+#define THRESHOLD 50
+#define RATIO 3
+
 cv::Mat houghLinesTransform(cv::Mat sourceImage);
 
 std::vector<cv::Mat> splitImageInChunks(cv::Mat sourceImage, int numberChunks);
